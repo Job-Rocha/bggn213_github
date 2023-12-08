441bcc42b7b474f29e0499f1d40f3b9ed7bbbba3
Job Rocha <jrochahernandez@ucsd.edu>
2023-12-08 14:45:29 -0800

    .

---
+++ Class07/class_codes.R
@@ -0,0 +1,348 @@
+
+dim(x)
+#Data Import
+url <- "https://tinyurl.com/UK-foods"
+x <- read.csv(url)
+dim(x)
+head(x)
+rownames(x) <- x[,1]
+x <- x[,-1]
+head(x)
+rnorm(10)
+rnorm(10)
+?rnorm
+hist(rnorm(10000))
+?barplot
+hist(rnorm(10000), mean=3)
+hist(rnorm(10000, mean=3))
+tmp <- rnorm(30, 3)
+tmp
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+tmp
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+x
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+tmp <- c(rnorm(30, 3), rnorm(30, 3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+barplot(as.matrix(x), beside=F, col=rainbow(nrow(x)))
+k <- kmeans(x, centers=2, nstart=20)
+k <- kmeans(x, centers=2, nstart=20)
+k
+tmp <- c(rnorm(30, 3), rnorm(30, -3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+k$size
+k$cluster
+plot(x)
+colors <- c("red", "blue")
+plot(x, col=colors[k$cluster])
+colors <- c("red", "blue")
+plot(x, col=colors)
+colors <- c("red", "blue")
+k$cluster
+plot(x, col=colors[k$cluster])
+colors <- c("red", "blue")
+k$cluster
+plot(x, col=k$cluster)
+tmp <- c(rnorm(30, 3), rnorm(30, -3))
+x <- cbind(x=tmp, y=rev(tmp))
+plot(x)
+k <- kmeans(x, centers=2, nstart=20)
+k
+k$size
+colors <- c("red", "blue")
+k$cluster
+plot(x, col=k$cluster)
+colors <- c("red", "blue")
+plot(x, col=k$cluster)
+points(k$centers)
+colors <- c("red", "blue")
+plot(x, col=k$cluster)
+points(k$centers, col="blue", pch=15, cex=2)
+k3 <- kmeans(x, centers=2, nstart=20)
+plot(x, col=k3$cluster)
+points(k3$centers, col="blue", pch=15, cex=2)
+k3 <- kmeans(x, centers=3, nstart=20)
+plot(x, col=k3$cluster)
+points(k3$centers, col="blue", pch=15, cex=2)
+k$centers
+pairs(x, col=rainbow(10), pch=16)
+hclust(dist(x))
+plot(hc)
+hc <- hclust(dist(x))
+plot(hc)
+plot(hc)
+abline(h=8, col="red")
+cutree(hc, h=8)
+plot(hc, col=grps)
+grps <- cutree(hc, h=8)
+grps
+plot(hc, col=grps)
+plot(x, col=grps)
+dim(rna.data)
+url2 <- "https://tinyurl.com/expression-CSV"
+rna.data <- read.csv(url2, row.names=1)
+head(rna.data)
+dim(rna.data)
+dim(x)
+#Data Import
+url <- "https://tinyurl.com/UK-foods"
+x <- read.csv(url)
+dim(x)
+head(x)
+rownames(x) <- x[,1]
+x <- x[,-1]
+head(x)
+dim(x)
+dim(x)
+x <- read.csv(url, row.names=1)
+head(x)
+barplot(as.matrix(x), beside=T, col=rainbow(nrow(x)))
+barplot(as.matrix(x), beside=F, col=rainbow(nrow(x)))
+pairs(x, col=rainbow(10), pch=16)
+pca <- prcomp( t(x) )
+summary(pca)
+# Plot PC1 vs PC2
+plot(pca$x[,1], pca$x[,2], xlab="PC1", ylab="PC2", xlim=c(-270,500))
+text(pca$x[,1], pca$x[,2], colnames(x))
+plot(pca$x[,1], pca$x[,2], xlab="PC1", ylab="PC2", xlim=c(-270,500))
+text(pca$x[,1], pca$x[,2], colnames(x), col=rainbow(nrow(x)))
+plot(pca$x[,1], pca$x[,2], xlab="PC1", ylab="PC2", xlim=c(-270,500), col=rainbow(nrow(x)))
+text(pca$x[,1], pca$x[,2], colnames(x), col=rainbow(nrow(x)))
+v <- round( pca$sdev^2/sum(pca$sdev^2) * 100 )
+v
+## or the second row here...
+z <- summary(pca)
+z$importance
+barplot(v, xlab="Principal Component", ylab="Percent Variation")
+## Lets focus on PC1 as it accounts for > 90% of variance
+par(mar=c(10, 3, 0.35, 0))
+barplot( pca$rotation[,1], las=2 )
+par(mar=c(10, 3, 0.35, 0))
+barplot( pca$rotation[,2], las=2 )
+library(ggplot2)
+df <- as.data.frame(pca$x)
+df_lab <- tibble::rownames_to_column(df, "Country")
+# Our first basic plot
+ggplot(df_lab) +
+  aes(PC1, PC2, col=Country) +
+  geom_point()
+ggplot(df_lab) +
+  aes(PC1, PC2, col=Country, label=Country) +
+  geom_hline(yintercept = 0, col="gray") +
+  geom_vline(xintercept = 0, col="gray") +
+  geom_point(show.legend = FALSE) +
+  geom_label(hjust=1, nudge_x = -10, show.legend = FALSE) +
+  expand_limits(x = c(-300,500)) +
+  xlab("PC1 (67.4%)") +
+  ylab("PC2 (28%)") +
+  theme_bw()
+ld <- as.data.frame(pca$rotation)
+ld_lab <- tibble::rownames_to_column(ld, "Food")
+ggplot(ld_lab) +
+  aes(PC1, Food) +
+  geom_col()
+ggplot(ld_lab) +
+  aes(PC1, reorder(Food, PC1), bg=PC1) +
+  geom_col() +
+  xlab("PC1 Loadings/Contributions") +
+  ylab("Food Group") +
+  scale_fill_gradient2(low="purple", mid="gray", high="darkgreen", guide=NULL) +
+  theme_bw()
+## The inbuilt biplot() can be useful for small datasets
+biplot(pca)
+url2 <- "https://tinyurl.com/expression-CSV"
+rna.data <- read.csv(url2, row.names=1)
+head(rna.data)
+dim(rna.data)
+## Again we have to take the transpose of our data
+pca <- prcomp(t(rna.data), scale=TRUE)
+## Simple un polished plot of pc1 and pc2
+plot(pca$x[,1], pca$x[,2], xlab="PC1", ylab="PC2")
+## Again we have to take the transpose of our data
+pca <- prcomp(t(rna.data), scale=TRUE)
+## Simple un polished plot of pc1 and pc2
+plot(pca$x[,1], pca$x[,2], xlab="PC1", ylab="PC2")
+summary(pca)
+plot(pca, main="Quick scree plot")
+## Variance captured per PC
+pca.var <- pca$sdev^2
+## Percent variance is often more informative to look at
+pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
+pca.var.per
+barplot(pca.var.per, main="Scree Plot",
+        names.arg = paste0("PC", 1:10),
+        xlab="Principal Component", ylab="Percent Variation")
+## A vector of colors for wt and ko samples
+colvec <- colnames(rna.data)
+colvec[grep("wt", colvec)] <- "red"
+colvec[grep("ko", colvec)] <- "blue"
+plot(pca$x[,1], pca$x[,2], col=colvec, pch=16,
+     xlab=paste0("PC1 (", pca.var.per[1], "%)"),
+     ylab=paste0("PC2 (", pca.var.per[2], "%)"))
+text(pca$x[,1], pca$x[,2], labels = colnames(rna.data), pos=c(rep(4,5), rep(2,5)))
+library(ggplot2)
+df <- as.data.frame(pca$x)
+# Our first basic plot
+ggplot(df) +
+  aes(PC1, PC2) +
+  geom_point()
+# Add a 'wt' and 'ko' "condition" column
+df$samples <- colnames(rna.data)
+df$condition <- substr(colnames(rna.data),1,2)
+p <- ggplot(df) +
+  aes(PC1, PC2, label=samples, col=condition) +
+  geom_label(show.legend = FALSE)
+p
+p + labs(title="PCA of RNASeq Data",
+         subtitle = "PC1 clealy seperates wild-type from knock-out samples",
+         x=paste0("PC1 (", pca.var.per[1], "%)"),
+         y=paste0("PC2 (", pca.var.per[2], "%)"),
+         caption="Class example data") +
+  theme_bw()
+loading_scores <- pca$rotation[,1]
+## Find the top 10 measurements (genes) that contribute
+## most to PC1 in either direction (+ or -)
+gene_scores <- abs(loading_scores)
+gene_score_ranked <- sort(gene_scores, decreasing=TRUE)
+## show the names of the top 10 genes
+top_10_genes <- names(gene_score_ranked[1:10])
+top_10_genes
+rownames(x) <- x[,1]
+x <- x[,-1]
+head(x)
+gc()
+gc()
